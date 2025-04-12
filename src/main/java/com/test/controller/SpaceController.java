bab7db1a5c6f7189af3298bd479b68a7743df066
Ibrahim Char <ibrachar68@gmail.com>
2025-04-12 12:20:46 +0200

    setup entity Point, repository,service and spaceController

---
+++ src/main/java/com/test/controller/SpaceController.java
@@ -0,0 +1,9 @@
+package com.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/space")
+public class SpaceController {
+}
---
+++ src/main/java/com/test/entity/Point.java
@@ -0,0 +1,19 @@
+package com.test.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "points")
+@Getter
+@Setter
+public class Point {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int x;
+    private int y;
+
+}
